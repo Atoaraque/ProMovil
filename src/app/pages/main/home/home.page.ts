a1d83421422b7f1d05f0837564b4b01df72b734b
Dasomacha <dannamarinch@gmail.com>
2024-08-31 20:25:54 -0500

    Home Admin

--- src/app/pages/main/home/home.page.ts
+++ src/app/pages/main/home/home.page.ts
@@ -5,52 +5,67 @@ import { FirebaseService } from 'src/app/services/firebase.service';
 import { UtilsService } from 'src/app/services/utils.service';
 import { AddUpdateProductComponent } from 'src/app/shared/components/add-update-product/add-update-product.component';
 import { orderBy, where } from "firebase/firestore";
-
+ 
 @Component({
   selector: "app-home",
   templateUrl: './home.page.html',
   styleUrls: ['/home.page.scss'],
 })
 export class HomePage implements OnInit {
-
+ 
   firebaseSvc = inject(FirebaseService);
   utilsSvc = inject(UtilsService);
-
+ 
   products: Product[] = [];
   loading: boolean = false;
-
+ 
   ngOnInit() {
   }
-  
+ 
   signOut(){
     this.firebaseSvc.signOut();
   }
   
+  user(): User {
+    return this.utilsSvc.getFromLocalStorage('user');
+  }
+  ionViewWillEnter() {
+    this.getProducts();
+  }
+ 
+  doRefresh(event) {
+    setTimeout(() => {
+      this.getProducts();
+      event.target.complete();
+    }, 1000);
+  }
+ 
+  // Obtener productos //
   getProducts() {
     let path = `users/${this.user().uid}/products`;
-
+ 
     this.loading = true;
-
+ 
     let query = [
       orderBy('soldUnits', 'desc'),
       // where('soldUnits','>', 30)
     ]
-
+ 
     let sub = this.firebaseSvc.getCollectionData(path, query).subscribe({
       next: (res: any) => {
         console.log(res);
         this.products = res;
-
+ 
         this.loading = false;
-
+ 
         sub.unsubscribe();
       }
     })
   }
-
+ 
   // Agregar o actualizar producto //
   async addUpdateProduct(product?: Product) {
-
+ 
     let success = await this.utilsSvc.presentModal({
       component: AddUpdateProductComponent,
       cssClass: 'add-update-modal',
@@ -58,8 +73,8 @@ export class HomePage implements OnInit {
     })
     if (success) this.getProducts();
   }
-
-
+ 
+ 
   // Confirmar eliminación del producto //
   async confirmDeleteProduct(product: Product) {
     this.utilsSvc.presentAlert({
@@ -78,22 +93,22 @@ export class HomePage implements OnInit {
       ]
     });
   }
-
+ 
   // Eliminar un producto //
   async deleteProduct(product: Product) {
-
+ 
     let path = `users/${this.user().uid}/products/${product.id}`
-
+ 
     const loading = await this.utilsSvc.loading();
     await loading.present();
-
+ 
     let imagePath = await this.firebaseSvc.getFilePath(product.image);
     await this.firebaseSvc.deleteFile(imagePath);
-
+ 
     this.firebaseSvc.deleteDocument(path).then(async res => {
-
+ 
       this.products = this.products.filter(p => p.id !== product.id);
-
+ 
       this.utilsSvc.presentToast({
         message: 'Producto eliminado exitosamente',
         duration: 1500,
@@ -101,10 +116,10 @@ export class HomePage implements OnInit {
         position: 'middle',
         icon: 'checkmark-circle-outline'
       })
-
+ 
     }).catch(error => {
       console.log(error);
-
+ 
       this.utilsSvc.presentToast({
         message: error.message,
         duration: 2500,
@@ -112,7 +127,7 @@ export class HomePage implements OnInit {
         position: 'middle',
         icon: 'alert-circle-outline'
       })
-
+ 
     }).finally(() => {
       loading.dismiss();
     }
