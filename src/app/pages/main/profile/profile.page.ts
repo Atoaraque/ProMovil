fe401f3ba5319ced8d9b332fc8f3a8373f3b2669
Alejandro Araque <j.alejandro.araque@gmail.com>
2024-09-03 21:07:30 -0500

    Cambios

--- src/app/pages/main/profile/profile.page.ts
+++ src/app/pages/main/profile/profile.page.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit, inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Product } from 'src/app/models/product.model';
 import { User } from 'src/app/models/user.model';
 import { FirebaseService } from 'src/app/services/firebase.service';
 import { UtilsService } from 'src/app/services/utils.service';
@@ -12,8 +14,8 @@ export class ProfilePage implements OnInit {
 
   firebaseSvc = inject(FirebaseService);
   utilsSvc = inject(UtilsService);
-  
-  ngOnInit() {}
+
+  ngOnInit() { }
 
   user(): User {
     return this.utilsSvc.getFromLocalStorage('user');
